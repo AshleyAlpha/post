ee0862c13cccfe932774d879521a12eb945e19dc
Wecode <mashleyalpha@gmail.com>
2019-02-28 10:49:11 +0200

    function that add comment in DB

--- app/models.py
+++ app/models.py
@@ -52,3 +52,9 @@ class Comment(db.Model):
     users_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     def __repr__(self):
         return f'User {self.comment}'
+def save_comment(self):
+        '''
+        Function that saves comments
+        '''
+        db.session.add(self)
+        db.session.commit()
